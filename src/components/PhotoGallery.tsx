8d850c3d878d460fd7ec294ed2280c7bec07a5c4
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2026-04-13 19:22:44 +0530

    some more checks

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -17,7 +17,7 @@ const categories: Category[] = ["Architecture", "Urbanscape", "Landscape", "Natu
 const photos: PhotoItem[] = [
   // Architecture
   {
-    src: "public/photos/architecture/architecture-photo1.webp",
+    src: "dist/photos/architecture/architecture-photo1.webp",
     alt: "Narasimha killing Hiranyakaśipu",
     category: "Architecture",
     location: "Halebeedu, Karnataka",
